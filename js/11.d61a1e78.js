1f9b9f5e9165f65d9b7de66b72506f502f37edaa
Rafi Harumantaka <rafiharumantaka16@gmail.com>
2020-04-27 22:24:29 +0700

    ee1887d

---
+++ js/11.d61a1e78.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[11],{"311b":function(e,t,n){"use strict";n.r(t);var a=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("q-page",{staticClass:"justify-center"},[n("h1",[e._v("Settings Page")])])},s=[],c={name:"PageSettings"},i=c,r=n("2877"),u=Object(r["a"])(i,a,s,!1,null,"155600a8",null);t["default"]=u.exports}}]);
